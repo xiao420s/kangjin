7d98662fcfc73933c81a71024d98a1343f7bb3d7
xiao420s <296816455@qq.com>
2017-03-14 08:53:45 +0800

    change admin.js

--- router/admin.js
+++ router/admin.js
@@ -3,27 +3,24 @@ const router=express.Router();
 const path=require('path');
 const pool=require('../mysql.js');
 const multer=require('multer');
-const upload=multer({dest:'upload/'});
+const upload=multer({dest:'./upload/'});
 const fs=require('fs');
 const async=require('async');
 
 router.get('/',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/admin_product.html"));
-    // res.sendFile(path.resolve('./views/admin/admin.html'));
 });
-router.post('/upload',upload.single('file'),(req,res)=>{
-    /*console.log(req.file);
-    const o=fs.createWriteStream(path.resolve('./upload',req.file.originalname))
-    fs.createReadStream(path.resolve(req.file.path)).pipe(o);
-    o.on('finish',()=>{
-        fs.unlink(path.resolve(req.file.path));
-    })*/
+router.post('/upload',upload.single('wangEditorH5File'),(req,res)=>{
+    console.log(req.file);
+    const filename=req.file.filename+'.'+(req.file.mimetype.split('/')[1]);
     async.series([(callback)=>{
-        fs.createReadStream(path.resolve(req.file.path)).pipe(fs.createWriteStream(path.resolve('./upload',req.file.originalname)));
+        fs.createReadStream(path.resolve(req.file.path)).pipe(fs.createWriteStream(path.resolve('./public/product',filename)));
         callback(null);
     },(callback)=>{
         fs.unlink(path.resolve(req.file.path));
         callback(null);
+    },(callback)=>{
+        res.end(`http://localhost:3000/product/${filename}`);
     }])
 });
 ////////////////////
@@ -42,7 +39,9 @@ router.post('/news/updateCate',(req,res)=>{
 });
 router.get('/news/del/:cate_id',(req,res)=>{
     pool('delete from content_cates where cate_id=?',[req.params.cate_id],(err,result)=>{
-        res.json(result);
+        if(!err){
+            res.redirect('/admin/news')
+        }
     })
 });
 router.get('/news/add',(req,res)=>{
@@ -53,6 +52,38 @@ router.get('/news/add',(req,res)=>{
 //////////////////////////
 router.get('/admin_news',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/admin_NewsList.html"));
+});
+router.get('/admin_news/newsall', (req, res)=> {
+    pool('select * from content order by c_id desc', (err, data)=> {
+        res.json(data);
+    })
+});
+router.get('/admin_news/add',(req,res)=>{
+    pool('insert into content (title,views,content,subtitle,cate_id) values ("","","","",1)',(err,result)=>{
+        // if (!err) {
+        //     res.redirect('/admin/admin_news');
+        // }
+        res.json(result.insertId);
+    })
+});
+router.get('/admin_news/updateCate/:c_id/:cate_id',(req,res)=>{
+    pool('update content set cate_id=? where c_id=?', [req.params.cate_id,req.params.c_id],(err, data)=> {
+        if(!err){
+            res.json('ok');
+        }
+    })
+});
+router.get('/admin_news/delete/:c_id', (req, res)=> {
+    pool('delete from content where c_id = ?',
+        [req.params.c_id], function (err, data) {
+            if (!err) {
+                res.redirect('/admin/admin_news');
+            }
+        })
+});
+//////////////////////////
+router.get('/admin_news/:cat_id',(req,res)=>{
+    res.sendFile(path.resolve("./views/admin/admin_newsContent.html"));
 });
 /////////////////////
 router.get('/product',(req,res)=>{
@@ -70,7 +101,9 @@ router.post('/product/updateCate',(req,res)=>{
 });
 router.get('/product/del/:cate_id',(req,res)=>{
     pool('delete from product_cates where cate_id=?',[req.params.cate_id],(err,result)=>{
-        res.json(result);
+        if(!err){
+            res.redirect('/admin/product')
+        }
     })
 });
 router.get('/product/add',(req,res)=>{
@@ -79,15 +112,113 @@ router.get('/product/add',(req,res)=>{
     })
 });
 /////////////////////
-router.get('/admin_news/:cat_id',(req,res)=>{
-    res.sendFile(path.resolve("./views/admin/admin_newsContent.html"));
-});
 router.get('/admin_productList',(req,res)=>{
-    res.sendFile(path.resolve("./views/admin/admin_LproductList.html"));
+    res.sendFile(path.resolve("./views/admin/admin_Lproductlist.html"));
 });
+router.get('/admin_productList/getAll',(req,res)=>{
+    pool('select id,name,cate_id,thumb from product order by id desc',(err,result)=>{
+        res.json(result);
+    })
+})
+router.get('/admin_productList/add',(req,res)=>{
+    pool('insert into product (name,cate_id,thumb) values ("",1,"")',(err,result)=>{
+        if(!err){
+            res.redirect('/admin/admin_productList')
+        }
+    })
+});
+router.get('/admin_productList/del/:id',(req,res)=>{
+    pool('delete from product where id=?',[req.params.id],(err,result)=>{
+        if(!err){
+            res.redirect('/admin/admin_productList')
+        }
+    })
+});
+router.get('/admin_productList/updateCate/:id/:value',(req,res)=>{
+    pool('update product set cate_id=? where id=?',[req.params.value,req.params.id],(err,result)=>{
+        if(!err){
+            res.json('ok');
+        }
+    })
+});
+router.post('/update_productList/:id',(req,res)=>{
+    pool('update product set name=?,ename=?,title=?,subtitle=?,fun=?,fun_content=?,fun_e_content=?,content=? where id=?',[req.body.name,req.body.ename,req.body.title,req.body.subtitle,req.body.fun,req.body.fun_content,req.body.fun_e_content,req.body.content,req.params.id],(err,result)=>{
+        if(!err){
+            res.json('ok');
+        }
+    })
+});
+//////////////////////////
 router.get('/admin_productList/:cat_id',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/admin_productCon.html"));
 });
+router.get('/admin_productList/:cat_id/get',(req,res)=>{
+    pool('select * from product where id=?',[req.params.cat_id],(err,result)=>{
+        res.json(result);
+    })
+});
+router.get('/admin_productList/:cat_id/getCrousel',(req,res)=>{
+    pool('select * from product_img where c_id=?',[req.params.cat_id],(err,result)=>{
+        res.json(result);
+    })
+});
+router.get('/admin_productList/:cat_id/getThumb',(req,res)=>{
+    pool('select id,thumb from product where id=?',[req.params.cat_id],(err,result)=>{
+        res.json(result);
+    })
+});
+router.get('/del/thumb/:id',(req,res)=>{
+    pool('update product set thumb="" where id=?',[req.params.id],(err,result)=>{
+        if(!err){
+            res.json('ok');
+        }
+    })
+});
+router.post('/upload/thumb/:id',upload.single('file'),(req,res)=>{
+    const filename=req.file.filename+'.'+(req.file.mimetype.split('/')[1]);
+    async.series([(callback)=>{
+        fs.createReadStream(path.resolve(req.file.path)).pipe(fs.createWriteStream(path.resolve('./public/product',filename)));
+        callback(null);
+    },(callback)=>{
+        fs.unlink(path.resolve(req.file.path));
+        callback(null);
+    },(callback)=>{
+        pool('update product set thumb=? where id=?',[`/product/${filename}`,req.params.id],(err,result)=>{
+            if(!err){
+                res.json({id:req.params.id,src:`/product/${filename}`});
+            }else{
+                res.json('err');
+            }
+        })
+    }])
+});
+router.post('/upload/crousel/:id',upload.single('file'),(req,res)=>{
+    const filename=req.file.filename+'.'+(req.file.mimetype.split('/')[1]);
+    async.series([(callback)=>{
+        fs.createReadStream(path.resolve(req.file.path)).pipe(fs.createWriteStream(path.resolve('./public/product',filename)));
+        callback(null);
+    },(callback)=>{
+        fs.unlink(path.resolve(req.file.path));
+        callback(null);
+    },(callback)=>{
+        pool('insert into product_img (c_id,img_url) values (?,?)',[req.params.id,`/product/${filename}`],(err,result)=>{
+            res.json({id:result.insertId,src:`/product/${filename}`});
+        })
+    }])
+});
+router.get('/del/crousel/:id',(req,res)=>{
+    pool('delete from product_img where i_id=?',[req.params.id],(err,result)=>{
+        if(!err){
+            res.json('ok');
+        }
+    })
+});
+router.get('/admin_productList/:cat_id/getCrousel',(req,res)=>{
+    pool('select * from product_img where c_id=?',[req.params.cat_id],(err,result)=>{
+        res.json(result);
+    })
+});
+//////////////////////////
 router.get('/admin_health',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/admin-industry.html"));
 });
@@ -100,7 +231,23 @@ router.get('/admin_recruit',(req,res)=>{
 router.get('/admin_recruit/:cat_id',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/admin_Lrecruit_details.html"));
 });
+/////////////////////////////////
 router.get('/admin_message',(req,res)=>{
     res.sendFile(path.resolve("./views/admin/message.html"));
 });
+//获取留言页面的信息
+router.get('/all', (req, res)=> {
+    pool('select * from message order by m_id desc', (err, data)=> {
+        res.json(data);
+    })
+});
+router.get('/admin_message/delete/:m_id', (req, res)=> {
+    pool('delete from message where m_id = ?',
+        [req.params.m_id], function (err, data) {
+            if (!err) {
+                res.redirect('/admin/admin_message');
+            }
+        })
+});
+/////////////////////
 module.exports=router;
