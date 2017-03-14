7ab0fe33ff1c0f8b74a5cff841522925d7d3f5d9
xiao420s <296816455@qq.com>
2017-03-14 08:49:14 +0800

    product admin

--- dev/admin_Lproductlist.jsx
+++ dev/admin_Lproductlist.jsx
@@ -1,86 +1,105 @@
-const React=require('react');
+const React = require('react');
 const ReactDOM=require('react-dom');
-const common=require('./admin_common.jsx');
-const MyHeader=common.Header;
-const MySide=common.Side;
-const MyFooter=common.Footer;
-const MyCrumbs=common.Crumbs;
-import { Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+const Nav=require('./admin_nav.jsx');
 //产品列表页部分
 import { Card } from 'antd';
 import { Select } from 'antd';
+import { Button } from 'antd';
+import { Pagination } from 'antd';
 const Option = Select.Option;
-function handleChange(value) {
-    console.log(`selected ${value}`);
-}
-const data=[
-    {id:1,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/1'},
-    {id:2,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/2'},
-    {id:3,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/3'},
-    {id:4,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/4'},
-    {id:5,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/5'},
-    {id:6,name:'粉红色的',pic:'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',href:'/admin/admin_productList/6'}]
 class Lproductlist extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            data:[]
+        };
+        this.handleChange=this.handleChange.bind(this)
+    }
+    componentDidMount(){
+        fetch('/admin/product/getCates',{credentials: 'same-origin'}).then((res)=>res.json())
+            .then((data)=>{
+                this.setState({data:data});
+            })
+    }
+    handleChange(value) {
+        console.log(`selected ${value}`);
+        fetch(`/admin/admin_productList/updateCate/${this.props.data.id}/${value}`,{credentials: 'same-origin'}).then((res)=>res.json())
+         .then((data)=>{
+             if(data=='ok'){
+                 location.href='/admin/admin_productList'
+             }else {
+                 alert('更新错误');
+             }
+         })
+    }
     render(){
+        const options=this.state.data.map((v,i)=><Option value={v.cate_id} key={i}>{v.cate_name}</Option>)
         return(
             <div className="Lcardbox">
-                <Card >
+                <Card>
                     <div className="custom-image">
-                        <img alt="example" width="100%" src={this.props.data.pic}/>
+                        <img alt="example" width="100%" src={this.props.data.thumb}/>
                     </div>
                     <div className="custom-card">
                         <h3>ID: {this.props.data.id}</h3>
                         <h3>名称: {this.props.data.name}</h3>
                         <h3>所属分类:
-                            <Select defaultValue="lucy" style={{ width: 120 }} onChange={handleChange}>
-                                <Option value="jack">豆浆</Option>
-                                <Option value="lucy">传统豆制品</Option>
-                                <Option value="disabled">卤制品</Option>
-                                <Option value="Yiminghe">豆芽菜</Option>
+                            <Select value={this.props.data.cate_id} style={{ width: "120px",float:'right' }} onChange={this.handleChange}>
+                                {options}
                             </Select>
                         </h3>
 
                     </div>
                     <div>
-                        <a href={this.props.data.href}><Button className='Lxie' type="primary" style={{margin:'16px auto',height:'34px'}} >编辑</Button></a>
-                       <Button className='Ldel'type="danger" style={{margin:'16px 16px',height:'34px'}} >删除</Button>
+                        <a href={`/admin/admin_productList/${this.props.data.id}`}><Button className='Lxie' type="primary" style={{margin:'16px',height:'24px',paddingTop:'0',paddingBottom:"0"}} >编辑</Button></a>
+                        <a href={`/admin/admin_productList/del/${this.props.data.id}`}><Button className='Lxie' type="danger" style={{margin:'16px',height:'24px',paddingTop:'0',paddingBottom:"0"}} >删除</Button></a>
                     </div>
                 </Card>
             </div>
-
         )
     }
 }
-// 按钮部分
-import { Button, Dropdown, Icon } from 'antd';
-
 function handleMenuClick(e) {
     console.log('click', e);
 }
 class Admin extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            current: 1,
+            data:[]
+        }
+        this.onChange=this.onChange.bind(this);
+    }
+    onChange (page) {
+        console.log(page);
+        this.setState({
+            current: page,
+        });
+        this.add=this.add.bind(this);
+    }
+    componentDidMount(){
+        fetch('/admin/admin_productList/getAll',{credentials: 'same-origin'}).then((res)=>res.json())
+            .then((data)=>{
+                this.setState({
+                    data: data
+                });
+            })
+    }
     render(){
         return (
             <div>
-                <Layout>
-                    <Header style={{height:'80px',background:'#2b3643',padding:'0'}}><MyHeader/></Header>
-                    <Layout>
-                        <Sider><MySide/></Sider>
-                        <Content style={{background:"#fff"}}>
-                            <MyCrumbs/>
-                            <div className="Ladd">
-                                <Button type="primary" style={{margin:'10px auto',height:'34px'}} >添加</Button>
-                            </div>
-                            {/*<Lproductlist/>*/}
-                            {this.props.data.map((v,i)=><Lproductlist data={v} key={i}/>)}
-                        </Content>
-
-                    </Layout>
-                    <Footer style={{padding:0}}><MyFooter/></Footer>
-                </Layout>
+                <div className="Ladd">
+                    <a href="/admin/admin_productList/add">
+                        <Button type="primary" style={{marginBottom:'10px'}}>添加</Button>
+                    </a>
+                </div>
+                <div style={{overflow:'hidden'}}>
+                    {this.state.data.map((v,i)=><Lproductlist data={v} key={i}/>)}
+                </div>
+                <Pagination current={this.state.current} onChange={this.onChange} total={this.state.data.length} style={{margin:'10px auto'}}/>
             </div>
         )
     }
 }
-
-ReactDOM.render(<Admin data={data}/>,document.querySelector('#page'));
+ReactDOM.render(<Nav><Admin/></Nav>,document.querySelector('#page'));
--- dev/admin_productCon.jsx
+++ dev/admin_productCon.jsx
@@ -1,15 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Nav=require('./admin_nav.jsx');
+const Editor=require('./wangEditor.jsx')
 
-const common = require('./admin_common.jsx');
-const MyHeader = common.Header;
-const MySide = common.Side;
-const MyFooter = common.Footer;
-const MyCrumbs = common.Crumbs;
-
-
-import {Layout} from 'antd';
-const {Header, Footer, Sider, Content} = Layout;
 import {Input} from 'antd';
 import {Upload, Icon, Modal} from 'antd';
 import {Button} from 'antd';
@@ -20,15 +13,27 @@ class PicturesWall extends React.Component {
         this.state = {
             previewVisible: false,
             previewImage: '',
-            fileList: [{
-                uid: -1,
-                name: 'xxx.png',
-                status: 'done',
-                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-            }],
+            fileList: []
         };
-    }
+        this.handleCancel=this.handleCancel.bind(this);
+        this.handlePreview=this.handlePreview.bind(this);
+        this.handleChange=this.handleChange.bind(this);
 
+    }
+    componentDidMount(){
+        fetch(`/admin/admin_productList/${this.props.id}/getCrousel`,{credentials: 'same-origin'})
+            .then((res)=>res.json()).then((data)=>{
+            this.setState({fileList:data.map((v,i)=>{
+                var obj={
+                    uid: v.i_id,
+                    name: v.img_url.split('/')[2],
+                    status: 'done',
+                    url: v.img_url
+                }
+                return obj;
+            })})
+        })
+    }
 
     handleCancel() {
         this.setState({previewVisible: false})
@@ -44,6 +49,25 @@ class PicturesWall extends React.Component {
     handleChange({fileList}) {
         this.setState({fileList})
     };
+    handleRemove(e){
+        const flag=true;
+        if(e.response){
+            fetch(`/admin/del/crousel/${e.response.id}`,{credentials: 'same-origin'})
+                .then((res)=>res.json()).then((data)=>{
+                if(data!='ok'){
+                    const flag=false;
+                }
+            })
+        }else {
+            fetch(`/admin/del/crousel/${e.uid}`,{credentials: 'same-origin'})
+                .then((res)=>res.json()).then((data)=>{
+                if(data!='ok'){
+                    const flag=false;
+                }
+            })
+        }
+        return flag;
+    }
 
     render() {
         const {previewVisible, previewImage, fileList} = this.state;
@@ -56,13 +80,109 @@ class PicturesWall extends React.Component {
         return (
             <div className="clearfix">
                 <Upload
-                    action="/upload.do"
+                    action={`/admin/upload/crousel/${this.props.id}`}
                     listType="picture-card"
                     fileList={fileList}
                     onPreview={this.handlePreview}
                     onChange={this.handleChange}
+                    onRemove={this.handleRemove}
                 >
-                    {fileList.length >= 3 ? null : uploadButton}
+                    {fileList.length >= 4 ? null : uploadButton}
+                </Upload>
+                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{width: '100%'}} src={previewImage}/>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+class MyPicturesWall extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            previewVisible: false,
+            previewImage: '',
+            fileList: []
+        };
+        this.handleCancel=this.handleCancel.bind(this);
+        this.handlePreview=this.handlePreview.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+
+    }
+    componentDidMount(){
+        fetch(`/admin/admin_productList/${this.props.id}/getThumb`,{credentials: 'same-origin'})
+            .then((res)=>res.json()).then((data)=>{
+            console.log(data[0]);
+            console.log(data[0].thumb);
+                if(data[0].thumb){
+                    console.log(1);
+                    this.setState({fileList:data.map((v,i)=>{
+                        var obj={
+                            uid: v.id,
+                            name: v.thumb.split('/')[2],
+                            status: 'done',
+                            url: v.thumb
+                        }
+                        return obj;
+                    })})
+                }
+            })
+    }
+
+    handleCancel() {
+        this.setState({previewVisible: false})
+    } ;
+
+    handlePreview(file) {
+        this.setState({
+            previewImage: file.url || file.thumbUrl,
+            previewVisible: true,
+        });
+    };
+
+    handleChange({fileList}) {
+        this.setState({fileList})
+    };
+    handleRemove(e){
+        const flag=true;
+        if(e.response){
+            fetch(`/admin/del/thumb/${e.response.id}`,{credentials: 'same-origin'})
+                .then((res)=>res.json()).then((data)=>{
+                if(data!='ok'){
+                    const flag=false;
+                }
+            })
+        }else {
+            fetch(`/admin/del/thumb/${e.uid}`,{credentials: 'same-origin'})
+                .then((res)=>res.json()).then((data)=>{
+                if(data!='ok'){
+                    const flag=false;
+                }
+            })
+        }
+        return flag;
+    }
+
+    render() {
+        const {previewVisible, previewImage, fileList} = this.state;
+        const uploadButton = (
+            <div>
+                <Icon type="plus"/>
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
+        return (
+            <div className="clearfix">
+                <Upload
+                    action={`/admin/upload/thumb/${this.props.id}`}
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={this.handlePreview}
+                    onChange={this.handleChange}
+                    onRemove={this.handleRemove}
+                >
+                    {fileList.length >= 1 ? null : uploadButton}
                 </Upload>
                 <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
                     <img alt="example" style={{width: '100%'}} src={previewImage}/>
@@ -73,77 +193,108 @@ class PicturesWall extends React.Component {
 }
 
 class ProductContent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            data:[]
+        };
+        this.save=this.save.bind(this);
+    }
+    componentDidMount(){
+        const href=location.href.split('/');
+        const id=href[href.length-1];
+        fetch(`/admin/admin_productList/${id}/get`,{credentials: 'same-origin'}).then((res)=>res.json())
+            .then((data)=>{
+                this.setState({data:data});
+            })
+    }
+    save(x){
+        var values={
+            name:this.name.refs.input.value,
+            ename:this.ename.refs.input.value,
+            title:this.title.refs.input.value,
+            subtitle:this.subtitle.refs.input.value,
+            fun:this.fun.refs.input.value,
+            fun_content:this.fun_content.refs.input.value,
+            fun_e_content:this.fun_e_content.refs.input.value,
+            content:x
+        };
+        const href=location.href.split('/');
+        const index=href[href.length-1];
+        fetch(`/admin/update_productList/${index}`,{
+            credentials: 'same-origin',
+            method:'post',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(values)
+        }).then((res)=>res.json()).then((data)=>{
+            if(data=="ok"){
+                location.href="/admin/admin_productList";
+            }else{
+                alert('更新失败');
+            }
+        })
+    }
     render() {
-        return (
-            <div className="NewsContent">
-                <div>
+        const div=this.state.data.map((v,i)=>(
+            <div key={i}>
+                <div className="top">
                     <div className="proID">
-                        <span>ID：</span>
-                        <Input placeholder="ID"/>
+                        <span>ID：{v.id}</span>
                     </div>
                     <div className="proName">
                         <span>产品名称：</span>
-                        <Input placeholder="产品名称"/>
+                        <Input placeholder="产品名称" defaultValue={v.name} name="name" ref={(el)=>{this.name=el}}/>
+                    </div>
+                    <div className="enTitle">
+                        <span>英文名：</span>
+                        <Input placeholder="英文名" defaultValue={v.ename} name="ename" ref={(el)=>{this.ename=el}}/>
                     </div>
                 </div>
-                <div className="enTitle">
-                    <span>英文名：</span>
-                    <Input placeholder="英文名"/>
+                <div className="proCarouselImg">
+                    <span>上传缩略图：</span>
+                    <MyPicturesWall id={v.id}/>
                 </div>
                 <div className="mainTitle">
                     <span>主标题：</span>
-                    <Input placeholder="主标题"/>
+                    <Input placeholder="主标题" defaultValue={v.title} name="title" ref={(el)=>{this.title=el}}/>
                 </div>
                 <div className="subTitle">
                     <span>副标题：</span>
-                    <Input placeholder="副标题"/>
+                    <Input placeholder="副标题" defaultValue={v.subtitle} name="subtitle" ref={(el)=>{this.subtitle=el}}/>
                 </div>
                 <div className="role">
                     <span>功能：</span>
-                    <Input placeholder="功能"/>
+                    <Input placeholder="功能" defaultValue={v.fun} name="fun" ref={(el)=>{this.fun=el}}/>
                 </div>
                 <div className="chAbstract">
                     <span>中文描述：</span>
-                    <Input type="textarea"/>
+                    <Input type="textarea"  defaultValue={v.fun_content} name="fun_content" ref={(el)=>{this.fun_content=el}}/>
                 </div>
                 <div className="enAbstract">
                     <span>英文描述：</span>
-                    <Input type="textarea"/>
+                    <Input type="textarea" defaultValue={v.fun_e_content} name="fun_e_content" ref={(el)=>{this.fun_e_content=el}}/>
                 </div>
                 <div className="proCarouselImg">
                     <span>上传轮播图：</span>
-                    <PicturesWall />
+                    <PicturesWall id={v.id}/>
                 </div>
                 <div className="proImg">
-                    <span>内容图片：</span>
-                    <PicturesWall />
+                    内容：
+                    <Editor.Editor style={{height:'300px'}} content={v.content} save={this.save}/>
                 </div>
+            </div>
+        ))
+        return (
+            <div className="NewsContent">
+                {div}
                 <div className="newsButton">
-                    <Button type="primary">保存</Button>
-                    <Button type="danger">返回</Button>
+                    <a href="/admin/admin_productList"><Button type="danger" style={{position:'relative',left:'40px',top:'-38px'}}>返回</Button></a>
                 </div>
             </div>
         );
     }
 }
-
-class Admin extends React.Component {
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <Header style={{height: '80px', background: '#2b3643', padding: '0'}}><MyHeader/></Header>
-                    <Layout>
-                        <Sider><MySide/></Sider>
-                        <Content style={{background: "#fff"}}>
-                            <MyCrumbs/>
-                            <ProductContent/>
-                        </Content>
-                    </Layout>
-                    <Footer style={{padding: 0}}><MyFooter/></Footer>
-                </Layout>
-            </div>
-        )
-    }
-}
-ReactDOM.render(<Admin/>, document.querySelector('#productCon'));
+ReactDOM.render(<Nav><ProductContent/></Nav>, document.querySelector('#productCon'));
